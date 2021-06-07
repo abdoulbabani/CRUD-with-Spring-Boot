83d0c0812d09f4aba9ef22407a85679632dbb5d5
AbdoulKarim <garba.babani1@gmail.com>
2021-06-08 00:03:18 +0100

    use of the BCryptPasswordEncoder class to encode the one-way password

--- src/main/java/org/sid/security/SecurityConfig.java
+++ src/main/java/org/sid/security/SecurityConfig.java
@@ -1,20 +1,26 @@
 package org.sid.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	
+	
 	 @Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		 
+		 PasswordEncoder passwordEncoder=passwordEncoder();
 		
-		 auth.inMemoryAuthentication().withUser("user1").password("{noop}1234").roles("user");
-		 auth.inMemoryAuthentication().withUser("user2").password("{noop}1234").roles("user");
-		 auth.inMemoryAuthentication().withUser("user3").password("{noop}1234").roles("user","ADMIN");
+		 auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("1234")).roles("user");
+		 auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("1234")).roles("user");
+		 auth.inMemoryAuthentication().withUser("user3").password(passwordEncoder.encode("1234")).roles("user","ADMIN");
 		 
 		 
 	}
@@ -30,6 +36,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.csrf();
 	}
 	
+	@Bean
 	
+	public PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
 
 }
