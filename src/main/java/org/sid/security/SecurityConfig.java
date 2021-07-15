8f4ac30c8b0fcb6a79706fd2812d5dd8103dafa6
AbdoulKarim <garba.babani1@gmail.com>
2021-07-15 11:22:25 +0100

    use of the anonymous dry method for spring security

--- src/main/java/org/sid/security/SecurityConfig.java
+++ src/main/java/org/sid/security/SecurityConfig.java
@@ -25,10 +25,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
+ 
+		/* cette requette permet d afficher la page login*/
 		http.formLogin();
+		
+		/* cette requette permet de specifier que seul admin est authauriser a la faire*/
+		
 		http.authorizeRequests().antMatchers("/save**/**", "/delesupprimer**/**", "/Ajouter**/**").hasRole("ADMIN");
-		http.authorizeRequests().anyRequest().authenticated();
+		
+		/* cette requette permet de specifier que seul Utilisateur User est authauriser a la faire*/
+		
+		http.authorizeRequests().antMatchers("/index**/**").hasRole("user");
+		
+		/**http.authorizeRequests().anyRequest().authenticated();
+		 * 
+		 */
 		http.csrf();
 
 		/* utiliser cette methode pour afficher les pages par defaut */
@@ -41,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	 * la classe new BCryptPasswordEncoder() return un objet de type PasswordEncoder
 	 * qui possede une methode encode() pour coder le mot de pass avant la
 	 * 
-	 * sauvegarde et l utilisation de BEAN veut dir qu on pourrait utiliser cette
+	 * sauvegarde et l utilisation de BEAN veut dire qu on pourrait utiliser cette
 	 * objet dans d autres class de l apllication apres l insjection de @Autowired
 	 */
 	  
